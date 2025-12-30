8ab6dd141a3c70267a35797a1a3ef6a502bd0f81
Agustín Viera <fagustinviera@gmail.com>
2025-12-30 11:29:44 -0300

    feat: add web-friendly navigation to pokemon detail

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,11 +1,19 @@
-import 'package:flutter/material.dart';
+import 'package:draftea_pokedex/feature/pokemon/presentation/pages/pokemon_detail_page.dart';
+import 'package:draftea_pokedex/feature/pokemon/presentation/pages/pokemon_list_page.dart';
 import 'package:go_router/go_router.dart';
 
 final pokedexRouter = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (_, __) => const Placeholder(),
+      builder: (_, __) => const PokemonListPage(),
+    ),
+    GoRoute(
+      path: '/pokemon/:id',
+      builder: (_, state) {
+        final id = int.parse(state.pathParameters['id']!);
+        return PokemonDetailPage(pokemonId: id);
+      },
     ),
   ],
 );
